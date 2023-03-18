59888c63d365d7be16280981746c29a6a642dff0
Halim Azeez <halim.azeze@googlemail.com>
2023-03-18 03:39:17 +0000

    add request from page to api wrote earlier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,16 @@
+import useSWR from 'swr';
+import { Launch } from '../interfaces';
+
 import styles from '../styles/Home.module.css';
 
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
 export default function Home() {
-  return <div className={styles.container}></div>;
+  const { data, error, isLoading } = useSWR<Launch[]>('/api/launches', fetcher);
+
+  console.log(data);
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (!data) return null;
+  return <div className={styles.container}>Hi</div>;
 }
