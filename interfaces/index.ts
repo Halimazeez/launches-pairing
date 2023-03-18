a5a141de90e9108c3a816b17f69001986ed612ea
Halim Azeez <halim.azeze@googlemail.com>
2023-03-18 22:29:49 +0000

    Launches API

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,6 +1,15 @@
 export type Launch = {
   id: string;
   name: string;
+  date: string;
+  core: string;
+  imageSrc: string;
+  status: boolean;
+  details: string;
+  payloads: {
+    id: string;
+    name: string;
+  }[];
 };
 
 export type ResponseError = {
--- pages/api/launches/index.ts
+++ pages/api/launches/index.ts
@@ -2,15 +2,31 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Launch } from '../../../interfaces';
 
 export default async function handler(_req: NextApiRequest, res: NextApiResponse<Launch[]>) {
+  // limit is auto set to 10 items on POST request
   const launchesRes = await fetch('https://api.spacexdata.com/v4/launches/query', {
     method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      options: {
+        populate: ['payloads'],
+      },
+    }),
   }).then((res) => res.json());
 
-  const launchesTyped = launchesRes.docs.map((launch: any) => ({
-    id: launch.id,
-    name: launch.name,
-  }));
+  // convert api json data to a frontend type for easability
+  const launchesTyped: Launch[] = launchesRes.docs.map(
+    (launch: any) =>
+      ({
+        id: launch.id,
+        name: launch.name,
+        date: launch.date_utc,
+        core: launch.cores[0].core,
+        imageSrc: launch.links.patch.small,
+        status: launch.success,
+        details: launch.details,
+        payloads: launch.payloads.map((payload: any) => ({ id: payload.id, name: payload.name })),
+      } as Launch)
+  );
 
   return res.status(200).json(launchesTyped);
-  // return res.status(200).json([{}] as Launch[]);
 }
