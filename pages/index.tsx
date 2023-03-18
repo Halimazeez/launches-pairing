222f4e8e52625eb6147d5727c7f1c4255ce70e0c
Halim Azeez <halim.azeze@googlemail.com>
2023-03-18 22:30:30 +0000

    Add main page index to call wrapper component with some styling additions to components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import useSWR from 'swr';
+import Launches from '../components/Launches';
 import { Launch } from '../interfaces';
 
 import styles from '../styles/Home.module.css';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+export const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Home() {
   const { data, error, isLoading } = useSWR<Launch[]>('/api/launches', fetcher);
 
-  console.log(data);
   if (error) return <div>Failed to load</div>;
-  if (isLoading) return <div>Loading...</div>;
-  if (!data) return null;
-  return <div className={styles.container}>Hi</div>;
+  if (isLoading || !data) return null;
+
+  return (
+    <div className={styles.main}>
+      <Launches launches={data} />
+    </div>
+  );
 }
