57908a82e8ca4d27599447758268045a8aa1a35b
Halim Azeez <halim.azeze@googlemail.com>
2023-03-20 14:00:49 +0000

    make axios post instead of get

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { Launch } from '../types/Launch';
 import styles from '../styles/Home.module.css';
 import axios from 'axios';
 
-export const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+export const fetcher = (url: string) => axios.post(url).then((res) => res.data);
 
 export default function Home() {
   const { data, error, isLoading } = useSWR<Launch[]>('/api/launches', fetcher);
